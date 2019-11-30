81460efccf726557b05d68ad041539f242d3d49d
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2019-11-30 19:08:54 +0100

    User email of AbstractUser

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+    AbstractUser._meta.get_field('email')._unique = True
     code = models.CharField(max_length=1000, blank=True, default='')
-
-    def __init__(self):
-        User._meta.get_field('email')._unique = True
 
     def __str__(self):
         return self.username
