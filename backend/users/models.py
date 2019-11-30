e28cb95aa1d1a65a662da72a448da4964287a685
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2019-11-30 19:21:53 +0100

    env folder added

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
     AbstractUser._meta.get_field('email')._unique = True
-    code = models.CharField(max_length=1000, blank=True, default='')
+    code = models.CharField(max_length=1000, null=True)
+    address = models.CharField(max_length=1000, null=True)
 
     def __str__(self):
         return self.username
