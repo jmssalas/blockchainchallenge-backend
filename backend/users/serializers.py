fd8a378b0a7c261037e413b128b7d05b20f736e6
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2019-11-30 16:35:31 +0100

    Registration with email and first_name

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,12 +5,14 @@ from users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'code']
+        fields = ['id', 'email', 'first_name', 'password', 'code']
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User(
-            username=validated_data['username']
+            username=validated_data['email'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name']
         )
         user.set_password(validated_data['password'])
         user.is_active = True
