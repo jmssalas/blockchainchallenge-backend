3c7bc1887ba054ace1181d03629aa2b8ec33bd9d
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2019-11-30 13:31:57 +0100

    Initial commit

---
+++ backend/backend/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
+
+API_V1 = 'api/v1/'
+
+urlpatterns = [
+    path(API_V1, include('users.urls')),
+]
+
+urlpatterns += [
+    path(API_V1 + 'auth/', obtain_jwt_token),
+]
