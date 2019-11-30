c66bfb7b2c12b731650bd94dcf03b63dc4f7811f
José María Sánchez Salas <josemaria.sanchezsalas@gmail.com>
2019-11-30 14:13:32 +0100

    User registration done

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class User(User):
+    code = models.CharField(max_length=1000, blank=True, default='')
+
+    def __str__(self):
+        return self.username
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from users.models import User
+from users.serializers import UserSerializer
 
-# Create your views here.
+
+class UserCreate(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
